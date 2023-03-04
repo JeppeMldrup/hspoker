0e474c7cd7afd41c41fdaf54321304e03ec457c0
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-03-04 15:20:10 +0100

    added get_hand function in the deck logic module

--- src/Deck_logic.hs
+++ src/Deck_logic.hs
@@ -1,5 +1,6 @@
 module Deck_logic (
     Card,
+    Hand,
     Deck,
     create_deck,
     shuffle,
@@ -14,6 +15,8 @@ data Value = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten 
 
 type Card = (Value, Suit)
 
+type Hand = (Card, Card)
+
 type Deck = [Card]
 
 create_deck :: Deck
@@ -24,6 +27,9 @@ shuffle x = if length x < 2 then return x else do
     i <- System.Random.randomRIO (0, length(x)-1)
     r <- shuffle (take i x ++ drop (i+1) x)
     return (x!!i : r)
+
+get_hand :: [a] -> (a, a)
+get_hand (x:xs) = (x, head xs)
 
 create_without :: [Card] -> [Card]
 create_without a = filter (\x -> (notElem x a)) create_deck
