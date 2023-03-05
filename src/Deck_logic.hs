8ac5d6752761b54264254984a8ed3582ac8958f4
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-03-05 09:49:49 +0100

    changed card to be a datatype and not just a mapping to a pair

--- src/Deck_logic.hs
+++ src/Deck_logic.hs
@@ -13,16 +13,23 @@ import System.Random
 
 data Suit = Clubs | Spades | Hearts | Diamonds deriving (Show, Enum, Eq);
 
-data Value = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King deriving (Show, Enum, Eq, Ord);
+data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Show, Enum, Eq, Ord);
 
-type Card = (Value, Suit)
+--type Card = (Value, Suit)
+data Card = Card Value Suit deriving (Show)
+
+instance Eq Card where
+    (Card va sa) == (Card vb sb) = va == vb
+
+instance Ord Card where
+    compare (Card va _) (Card vb _) = compare va vb
 
 type Hand = (Card, Card)
 
 type Deck = [Card]
 
 create_deck :: Deck
-create_deck = [(a, b) | a <- [Ace ..], b <- [Clubs ..]]
+create_deck = [Card a b | a <- [Ace ..], b <- [Clubs ..]]
 
 create_without :: [Card] -> [Card]
 create_without a = filter (\x -> (notElem x a)) create_deck
