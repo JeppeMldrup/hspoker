2aff1d53bfbea61bc82a92447abfbcda3965619a
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-03-08 17:06:04 +0100

    Added all the haddock comments to the testing framework module

--- Test/Framework.hs
+++ Test/Framework.hs
@@ -1,3 +1,7 @@
+{- |
+Module: Test
+Description: A testing framework for unit testing of haskell code
+-}
 module Test.Framework(
     UnitTest(..),
     TestResult(..),
@@ -21,19 +25,27 @@ Should be the result from the function in a UnitTest type
 -}
 data TestResult = Passed | Failed String deriving (Show)
 
+{- | Assert that two variables are equal, string is ID for the error message
+-}
 assertEqual str a b = if a == b then
     Passed
     else
         Failed ("Failed assertEqual: " ++ str ++ "\n" ++ "On values: " ++ (show a) ++ " " ++ (show b))
 
+{- | Assert that two variables are within a margin, string is ID for the error message
+-}
 assertEqualMargin str a b c = if a-b < c then Passed else Failed (str ++ " " ++ (show a) ++ " " ++ (show b))
 
+{- | To print the results of all the tests in a nice way
+-}
 printResults results = map printResult results
 
 printResult result = case result of
     Passed -> putStrLn "Passed test case"
     (Failed s) -> putStrLn s
 
+{- | To run all the unittests
+-}
 runTests tests = map runTest tests
 
 runTest (Test a b) = b a
