7d5a0f08c4fc1d7c17bed29eb15087045a755355
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-03-11 09:32:31 +0100

    Added a runAll function to the framework that takes a list of tests and runs them all and prints a human readable output with info about the test results

--- Test/Framework.hs
+++ Test/Framework.hs
@@ -7,8 +7,7 @@ module Test.Framework(
     TestResult(..),
     assertEqual,
     assertEqualMargin,
-    printResults,
-    runTests
+    runAll
 ) where
 
 import System.IO
@@ -31,11 +30,15 @@ data TestResult = Passed | Failed String deriving (Show)
 assertEqual str a b = if a == b then
     Passed
     else
-        Failed ("Failed assertEqual: " ++ str ++ "\n" ++ "On values: " ++ (show a) ++ " " ++ (show b))
+        Failed ("Failed assertEqual: " ++ str ++ "\n" ++ "On values:\n" ++ (show a) ++ "\n" ++ (show b))
 
 {- | Assert that two variables are within a margin, string is ID for the error message
 -}
-assertEqualMargin str a b c = if a-b < c then Passed else Failed (str ++ " " ++ (show a) ++ " " ++ (show b))
+assertEqualMargin str a b c =
+    if a-b < c then
+        Passed
+    else
+        Failed ("Failed assertEqualMargin: " ++ str ++ "\n" ++ "On values:\n" ++ (show a) ++ "\n" ++ (show b))
 
 {- | To print the results of all the tests in a nice way
 -}
@@ -55,3 +58,17 @@ printResult result = case result of
 runTests tests = map runTest tests
 
 runTest (Test a b) = b a
+
+runAll tests = let results = runTests tests
+                   passed = filter (\x -> checkType x) results
+                   failed = filter (\x -> not (checkType x)) results in
+    if (length failed) == 0 then
+        putStrLn "All tests passed"
+    else do
+        putStrLn ((show (length passed)) ++ " out of " ++ (show (length tests)) ++ " tests passed")
+        putStrLn ""
+        printResults failed
+
+checkType x = case x of
+    Passed -> True
+    Failed _ -> False
