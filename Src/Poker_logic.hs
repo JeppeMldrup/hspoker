196e27268cdc85742bab831e68eef0d1e4e80f77
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-04-06 14:08:04 +0200

    Added table and pot datatypes in the Poker_logic.hs file

--- Src/Poker_logic.hs
+++ Src/Poker_logic.hs
@@ -1,8 +1,15 @@
 module Src.Poker_logic (
-    Game
+    Table
 ) where
 
 import Src.Deck_logic
 import Src.Hand_logic
 
-data Game = Game Int Int 
+{-| Table datatype to keep track of entire games with deck of cards, list of hands, number of players and location of the button
+-}
+data Table = Table [Card] [(Card, Card)] Int Int
+
+{-| Pot datatype that keeps track of the total Pot, minimum bet and maximum bet
+-}
+data Pot = Pot Float Float Float
+
