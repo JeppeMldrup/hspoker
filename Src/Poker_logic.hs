dc1a5b1cdf4fa4fcd87db7aab992f7c97e4a0a8c
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-04-06 14:19:28 +0200

    Added a test for the addToPot function and made it pass

--- Src/Poker_logic.hs
+++ Src/Poker_logic.hs
@@ -1,5 +1,7 @@
 module Src.Poker_logic (
-    Table
+    Table(..),
+    Pot(..),
+    addToPot
 ) where
 
 import Src.Deck_logic
@@ -7,11 +9,14 @@ import Src.Hand_logic
 
 {-| Table datatype to keep track of entire games with deck of cards, list of hands, number of players and location of the button
 -}
-data Table = Table [Card] [(Card, Card)] Int Int
+data Table = Table [Card] [(Card, Card)] Int Int deriving (Show)
 
 {-| Pot datatype that keeps track of the total Pot, minimum bet and maximum bet
 -}
-data Pot = Pot Float Float Float
+data Pot = Pot Double Double Double deriving (Show)
+
+instance Eq Pot where
+    (Pot x1 y1 z1) == (Pot x2 y2 z2) = (x1 == x2 && y1 == y2 && z1 == z2)
 
 addToPot a (Pot x y z) = 
     if a <= z && a >= y then
--- Test/testPokerLogic.hs
+++ Test/testPokerLogic.hs
@@ -6,4 +6,8 @@ import Src.Poker_logic
 import Test.Framework
 
 test_pokerlogic = [
+    -- Test addToPot function
+    Test "Test addToPot function with less than min" (\x -> do
+        let pot = Pot 1.0 2.0 3.0
+        assertEqual x (addToPot 1 pot) pot)
     ]
