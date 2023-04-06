b11427d36a6116d0e0ad3fcb55d705c4ebe1853e
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-04-06 14:21:12 +0200

    Added two more tests for the addToPot function

--- Test/testPokerLogic.hs
+++ Test/testPokerLogic.hs
@@ -9,5 +9,11 @@ test_pokerlogic = [
     -- Test addToPot function
     Test "Test addToPot function with less than min" (\x -> do
         let pot = Pot 1.0 2.0 3.0
-        assertEqual x (addToPot 1 pot) pot)
+        assertEqual x (addToPot 1 pot) pot),
+    Test "Test addToPot function with more than max" (\x -> do
+        let pot = Pot 1 2 3
+        assertEqual x (addToPot 4 pot) pot),
+    Test "Test addToPot function with value between min and max" (\x -> do
+        let pot = Pot 1 2 3
+        assertEqual x (addToPot 2.5 pot) (Pot 3.5 2 3))
     ]
